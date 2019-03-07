8b9bdf643afc9a323c7f5a62ea1c52b9385d0a29
Jeffrey Tse <Jeffrey>
2019-03-06 22:50:53 -0800

    fix text direction

--- src/main/java/ServerOutput.java
+++ src/main/java/ServerOutput.java
@@ -7,12 +7,13 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ServerOutput extends Thread{
+    private static final int MAX_MESSAGES = 1000;
     protected Socket server;
-    protected ArrayList<String> messages;
+    protected String[] messages;
     public static volatile boolean welcomeAcknowledged = false;
     public ServerOutput(Socket server) {
         this.server = server;
-        this.messages = new ArrayList<>(1000);
+        this.messages = new String[MAX_MESSAGES];
     }
 
     public void run() {
@@ -34,8 +35,8 @@ public class ServerOutput extends Thread{
             render();
             int index = 0;
             while ((message = in.readLine()) != null) {
-                messages.add(index, message);
-                index = (index + 1) % 1000;
+                messages[index] = message;
+                index = (index + 1) % MAX_MESSAGES;
                 render();
             }
         } catch (IOException e) {
@@ -48,16 +49,19 @@ public class ServerOutput extends Thread{
     public void render() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-
-        for (String message : messages) {
-            if(message.equals("")){
-                System.out.println();
-            } else {
-                System.out.println(message);
-            }
-        }
         try{
             String seperator = new String(new char[org.jline.terminal.TerminalBuilder.terminal().getWidth()]).replace("\0", "-");
+            int numLines = org.jline.terminal.TerminalBuilder.terminal().getHeight() - 2;
+            int offset;
+            for(offset = 0; messages[offset] != null; offset++){
+            }
+            for (int i = (offset - numLines < 0 ? 0: offset - numLines); i < (offset - numLines < 0 ? numLines : offset); i++) {
+                if(messages[i] != null){
+                    System.out.println(messages[i]);
+                } else {
+                    System.out.println();
+                }
+            }
             System.out.println(seperator);
             System.out.print(ClientInput.username + ": ");
         } catch(IOException e){
